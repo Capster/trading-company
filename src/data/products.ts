9e09421cd428640f0660218078705029077ed80e
Maxim <Capster@users.noreply.github.com>
2023-10-22 02:38:26 +0300

    Fix the 'public/' assets path

--- src/data/products.ts
+++ src/data/products.ts
@@ -14,30 +14,30 @@ export const products: Array<Category> = [
     id: "drinks",
     name: "Напитки",
     content: [
-      { name: "Матча Латте Имбирный Gold Kili", image: "/products/matcha-latte-with-ginger.png" },
-      { name: "Имбирный чай Латте с мёдом Gold Kili", image: "/products/ginger-tea-latte.png" },
-      { name: "Матча Латте Gold Kili", image: "/products/matcha-latte.png" },
-      { name: "Имбирь натуральный Gold Kili", image: "/products/ginger-bag.png" },
+      { name: "Матча Латте Имбирный Gold Kili", image: "./products/matcha-latte-with-ginger.png" },
+      { name: "Имбирный чай Латте с мёдом Gold Kili", image: "./products/ginger-tea-latte.png" },
+      { name: "Матча Латте Gold Kili", image: "./products/matcha-latte.png" },
+      { name: "Имбирь натуральный Gold Kili", image: "./products/ginger-bag.png" },
     ],
   },
   {
     id: "marshmallow",
     name: "Маршмеллоу",
     content: [
-      { name: "Имбирь натуральный Gold Kili", image: "/products/ginger-bag.png" },
-      { name: "Матча Латте Имбирный Gold Kili", image: "/products/matcha-latte-with-ginger.png" },
-      { name: "Имбирный чай Латте с мёдом Gold Kili", image: "/products/ginger-tea-latte.png" },
-      { name: "Матча Латте Gold Kili", image: "/products/matcha-latte.png" },
+      { name: "Имбирь натуральный Gold Kili", image: "./products/ginger-bag.png" },
+      { name: "Матча Латте Имбирный Gold Kili", image: "./products/matcha-latte-with-ginger.png" },
+      { name: "Имбирный чай Латте с мёдом Gold Kili", image: "./products/ginger-tea-latte.png" },
+      { name: "Матча Латте Gold Kili", image: "./products/matcha-latte.png" },
     ],
   },
   {
     id: "candys",
     name: "Конфеты",
     content: [
-      { name: "Жевательный мармелад Ход Дог Gummi Zone", image: "/products/gummi-doggie.png" },
-      { name: "Имбирные жевательные конфеты с куркумой и манго FunBites", image: "/products/funbites-mango.png" },
-      { name: "Жевательный мармелад Бургер Gummi Zone", image: "/products/gummi-burger.png" },
-      { name: "Имбирные жевательные конфеты с куркумой и апельсином FunBites", image: "/products/funbites-orange.png" },
+      { name: "Жевательный мармелад Ход Дог Gummi Zone", image: "./products/gummi-doggie.png" },
+      { name: "Имбирные жевательные конфеты с куркумой и манго FunBites", image: "./products/funbites-mango.png" },
+      { name: "Жевательный мармелад Бургер Gummi Zone", image: "./products/gummi-burger.png" },
+      { name: "Имбирные жевательные конфеты с куркумой и апельсином FunBites", image: "./products/funbites-orange.png" },
     ],
   }
 ];
--- src/data/vendors.ts
+++ src/data/vendors.ts
@@ -6,36 +6,36 @@ interface Vendor {
 
 export const vendors: Array<Vendor> = [
   {
-    image: "/vendors/1.svg",
+    image: "./vendors/1.svg",
     url: "https://www.perekrestok.ru/",
   },
   {
-    image: "/vendors/2.svg",
+    image: "./vendors/2.svg",
     url: "https://av.ru/",
   },
   {
-    image: "/vendors/3.svg",
+    image: "./vendors/3.svg",
     url: "https://miratorg.ru",
   },
   {
-    image: "/vendors/4.png",
+    image: "./vendors/4.png",
     url: "https://mgnl.ru/",
     width: 7.83675,
   },
   {
-    image: "/vendors/5.svg",
+    image: "./vendors/5.svg",
     url: "https://www.globus.ru/",
   },
   {
-    image: "/vendors/6.svg",
+    image: "./vendors/6.svg",
     url: "https://magnit.ru/",
   },
   {
-    image: "/vendors/7.svg",
+    image: "./vendors/7.svg",
     url: "https://sbermarket.ru/",
   },
   {
-    image: "/vendors/8.svg",
+    image: "./vendors/8.svg",
     url: "https://market.yandex.ru/",
   },
 ];
